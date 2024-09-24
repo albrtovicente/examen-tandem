f8c7039e19edd5562eb317958795afe8bc2b0c90
albrtovicente <albervifetandem@gmail.com>
2024-09-24 09:05:11 +0200

    paso 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,25 +8,38 @@ import Card from "../componentes/Card"
 
 const Inicio = () => {
   
-    return (
-      <>
+  return (
+    <>
+      <div className="container">
+        <h1 style={{textAlign: `center`}}>Inicio</h1> 
+      <div >
+        <Link to="../contacto" style={{textAlign:`center`, fontSize:`34px`, textDecoration:`none`}}>
+          <p className="linkContacto">Contacto</p>
+        </Link>
+      </div>
+      <div className="cards" style={{textAlign:`center`}}>
+        <Card 
+          titulo="Gatsby"
+          imagen="../images/figma.jpg"
+          texto="dsvjdhbfbcscbhsb"
+        />
+        <Card
+          titulo="Gatsby"
+          imagen="../images/figma.jpg"
+          texto="dsvjdhbfbcscbhsb"
+        />
+        <Card
+          titulo="Gatsby"
+          imagen="../images/figma.jpg"
+          texto="dsvjdhbfbcscbhsb"
+        />
+      </div>
+      </div>
         
-         <h1 style={{textAlign: `center`}}>Inicio</h1> 
 
-         <Link to="../contacto">
-           Contacto
-         </Link>
-         <Card
-            titulo="Gatsby"
-            imagen="../images/figma.jpg"
-            texto="dsvjdhbfbcscbhsb"
-         />
-  
-          
-  
-      </>
-      
-    )
-  }
+    </>
+    
+  )
+}
   
   export default Inicio
